3a4234b93c260ef046a09a5d3660aab8a08298ba
Nick Carducci <nmcarducci@gmail.com>
2022-11-06 12:25:14 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -9,8 +9,9 @@ export default {
         "https://wavv.art"
       ];
 
-      const urlObject = new URL(request.url); //.pathname;//path
-      var origin = urlObject.origin; // request.headers.get("Origin");
+      //const urlObject = new URL(request.url); //.pathname;//path
+      //var origin = urlObject.origin;
+      var origin = request.headers.get("Origin");
       const dataHead = {
         "Access-Control-Allow-Origin": origin, //new URL(req.url),//
         "Access-Control-Allow-Headers": [
