4adebf5decdf26ff7cd7a0fce5ab264d0e1b85de
Nick Carducci <nmcarducci@gmail.com>
2022-11-06 12:13:47 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,56 +1,58 @@
 export default {
-    fetch(request/*, env, ctx*/) {
-        //Response class must be a promise
-        try {
-            var allowedOrigins = [
-                "https://h0u8vv.csb.app",
-                "https://commie.dev",
-                "https://4vosh.csb.app",
-                "https://wavv.art"
-            ];
+  fetch(request /*, env, ctx*/) {
+    //Response class must be a promise
+    try {
+      var allowedOrigins = [
+        "https://h0u8vv.csb.app",
+        "https://commie.dev",
+        "https://4vosh.csb.app",
+        "https://wavv.art"
+      ];
 
-            const urlObject = new URL(request.url); //.pathname;//path
-            var origin = urlObject.origin; // request.headers.get("Origin");
-            const dataHead = {
-                "Access-Control-Allow-Origin": origin,//new URL(req.url),//
-                "Access-Control-Allow-Headers": [
-                    "Access-Control-Request-Headers",
-                    //"Access-Control-Allow-Methods",
-                    "Content-Type",
-                    "Origin",
-                    "Referer",
-                ],
-                "Access-Control-Allow-Methods": ["GET", "POST", "OPTIONS"]
-            }
-            if (allowedOrigins.indexOf(origin) === -1) return new Response(
-                `{error:${"no access for this origin- " + origin}}`,
-                {
-                    status: 400,
-                    message: "no access for this origin: " + origin
-                    //headers: { "Content-Type": "application/json" }
-                }
-            );
+      const urlObject = new URL(request.url); //.pathname;//path
+      var origin = urlObject.origin; // request.headers.get("Origin");
+      const dataHead = {
+        "Access-Control-Allow-Origin": origin, //new URL(req.url),//
+        "Access-Control-Allow-Headers": [
+          "Access-Control-Request-Headers",
+          //"Access-Control-Allow-Methods",
+          "Content-Type",
+          "Origin",
+          "Referer"
+        ],
+        "Access-Control-Allow-Methods": ["GET", "POST", "OPTIONS"]
+      };
+      if (allowedOrigins.indexOf(origin) === -1)
+        return new Response(
+          `{error:${"no access for this origin- " + origin}}`,
+          {
+            status: 400,
+            message: "no access for this origin: " + origin
+            //headers: { "Content-Type": "application/json" }
+          }
+        );
 
-            if (request.method === "OPTIONS"){
-                console.log("OPTIONS for "+origin)
-                return new Response(`preflight response for POST`, {
-                    status: 200,
-                    message: `preflight response for POST`,
-                    headers
-                });}
-                console.log("POSTing from "+origin)
-            return new Response(`{data:${snipcartkey}}`, {
-                status: 200,
-                message: "success",
-                headers: {
-                    ...dataHead,
-                    "Content-Type": "application/json"
-                }
-            });
-            // wrap the body of your callback in a try/catch block to ensure it cannot throw an exception.
-            // is return, "the body?"
-        } catch (e) {
-            return new Response(e.message);
+      if (request.method === "OPTIONS") {
+        console.log("OPTIONS for " + origin);
+        return new Response(`preflight response for POST`, {
+          status: 200,
+          message: `preflight response for POST`,
+          headers: dataHead
+        });
+      }
+      console.log("POSTing from " + origin);
+      return new Response(`{data:${snipcartkey}}`, {
+        status: 200,
+        message: "success",
+        headers: {
+          ...dataHead,
+          "Content-Type": "application/json"
         }
+      });
+      // wrap the body of your callback in a try/catch block to ensure it cannot throw an exception.
+      // is return, "the body?"
+    } catch (e) {
+      return new Response(e.message);
     }
+  }
 };
